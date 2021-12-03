80f979bc339cbc17799a3eab84cda1710b552f13
achdiathadit <achdiat.dev@gmail.com>
2021-12-03 19:00:23 +0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="q-pa-md">
+    <q-table
+      title="Biodata"
+      :rows="tableData"
+      :columns="tableColumns"
+      row-key="name"
+    />
+  </div>
   </q-page>
 </template>
 
@@ -12,6 +15,47 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      tableColumns: [
+        {
+          name: 'name',
+          label: 'Name',
+          align: 'left',
+          field: 'name',
+          sortable: true
+        },
+        {
+          name: 'age',
+          label: 'Age',
+          align: 'left',
+          field: 'age',
+          sortable: true
+        },
+        {
+          name: 'hobbies',
+          label: 'Hobbies',
+          align: 'left',
+          field: 'hobbies',
+          sortable: true
+        },
+      ],
+      tableData: []
+    }
+  },
+  mounted () {
+    this.getTableData()
+  },
+  methods: {
+    getTableData () {
+      this.$axios.get('http://demo2725834.mockable.io/api/v1/list').then((res) => {
+          this.tableData = res.data.data;
+          console.log(res.data.data);
+        }).catch((err) => {
+          console.error(err)
+        })
+    }
+  }
 })
 </script>
